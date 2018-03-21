f24d77e7102e471541087caacd5bd0c54b6f9e94
Ratthanan <ratthanan@wongnai.com>
2018-03-21 22:14:12 +0700

    Also add data-qa when styling another component

--- src/index.js
+++ src/index.js
@@ -33,6 +33,42 @@ export default function ({ types: t }) {
         }
 
         if (t.isCallExpression(tag)) {
+          // styled(Component)``
+          if (t.isIdentifier(tag.callee) && tag.callee.name === 'styled') {
+            init.tag = t.callExpression(
+              t.memberExpression(tag, t.identifier('attrs')),
+              [
+                t.objectExpression([
+                  t.objectProperty(
+                    t.stringLiteral('data-qa'),
+                    t.stringLiteral(path.node.id.name)
+                  )
+                ])
+              ]
+            )
+          }
+
+          // styled(Component).attrs({})
+          if (
+            t.isMemberExpression(tag.callee) &&
+            t.isIdentifier(tag.callee.property) &&
+            tag.callee.property.name === 'attrs' &&
+            t.isCallExpression(tag.callee.object) &&
+            t.isIdentifier(tag.callee.object.callee) &&
+            tag.callee.object.callee.name === 'styled'
+          ) {
+            tag.arguments = [
+              t.objectExpression(
+                tag.arguments[0].properties.concat(
+                  t.objectProperty(
+                    t.stringLiteral('data-qa'),
+                    t.stringLiteral(path.node.id.name)
+                  )
+                )
+              )
+            ]
+          }
+
           // styled.div.attrs({})`` or styled[tag].attrs({})``
           if (
             t.isMemberExpression(tag.callee) &&
