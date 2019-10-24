0976a3c2c7b4476455f203f0285d59ac65384631
ratthanan <ratthanan@wongnai.com>
2019-10-24 10:29:21 +0700

    Add Prettier

--- src/options.js
+++ src/options.js
@@ -4,15 +4,15 @@ import kebabCase from 'lodash.kebabcase'
 import snakeCase from 'lodash.snakecase'
 
 export const getAttributeName = opts =>
-	isString(opts.attribute) ? opts.attribute : 'data-qa'
+  isString(opts.attribute) ? opts.attribute : 'data-qa'
 
 const formattersByName = {
-	camel: camelCase,
-	kebab: kebabCase,
-	snake: snakeCase,
+  camel: camelCase,
+  kebab: kebabCase,
+  snake: snakeCase,
 }
 
 export const getComponentNameFormatter = opts =>
-	isString(opts.format) && opts.format in formattersByName
-		? formattersByName[opts.format]
-		: kebabCase
+  isString(opts.format) && opts.format in formattersByName
+    ? formattersByName[opts.format]
+    : kebabCase
